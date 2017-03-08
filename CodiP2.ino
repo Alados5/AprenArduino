1b11c73ddbe478be443df983bd4ba19483d13ea0
Alados5 <adria.alados@gmail.com>
2017-03-08 19:38:50 +0100

    Create CodiP2.ino

---
+++ CodiP2.ino
@@ -0,0 +1,28 @@
+int estatPolsador=0;
+
+void setup() {
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  estatPolsador=digitalRead(2);
+
+  if (estatPolsador==LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }    
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(100);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+    delay(100);
+  }
+}
